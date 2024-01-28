a5c2b86b3a0c9783a0cd2fd4d65c79cf112307b7
旺仔 <14944603+HotKids@users.noreply.github.com>
2024-01-28 15:10:23 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -33,7 +33,13 @@ def parse_and_convert_to_dataframe(link):
         try:
             yaml_data = read_yaml_from_url(link)
             rows = []
-            for item in yaml_data.get('payload', []):
+            if not isinstance(yaml_data, str):
+                items = yaml_data.get('payload', [])
+            else:
+                lines = yaml_data.splitlines()
+                line_content = lines[0]
+                items = line_content.split()
+            for item in items:
                 address = item.strip("'")
                 if ',' not in item:
                     if is_ipv4_or_ipv6(item):
@@ -42,6 +48,8 @@ def parse_and_convert_to_dataframe(link):
                         if address.startswith('+'):
                             pattern = 'DOMAIN-SUFFIX'
                             address = address[1:]
+                            if address.startswith('.'):
+                                address = address[1:]
                         else:
                             pattern = 'DOMAIN'
                 else:
@@ -53,6 +61,17 @@ def parse_and_convert_to_dataframe(link):
     else:
         df = read_list_from_url(link)
     return df
+
+# 对字典进行排序，含list of dict
+def sort_dict(obj):
+    if isinstance(obj, dict):
+        return {k: sort_dict(obj[k]) for k in sorted(obj)}
+    elif isinstance(obj, list) and all(isinstance(elem, dict) for elem in obj):
+        return sorted([sort_dict(x) for x in obj], key=lambda d: sorted(d.keys())[0])
+    elif isinstance(obj, list):
+        return sorted(sort_dict(x) for x in obj)
+    else:
+        return obj
 
 def parse_list_file(link, output_directory):
     with concurrent.futures.ThreadPoolExecutor() as executor:
@@ -83,21 +102,37 @@ def parse_list_file(link, output_directory):
     os.makedirs(output_directory, exist_ok=True)
 
     result_rules = {"version": 1, "rules": []}
+    domain_entries = []
 
     for pattern, addresses in df.groupby('pattern')['address'].apply(list).to_dict().items():
-        rule_entry = {pattern: [address.strip() for address in addresses]}
-        result_rules["rules"].append(rule_entry)
+        if pattern == 'domain_suffix':
+            rule_entry = {pattern: ['.' + address.strip() for address in addresses]}
+            result_rules["rules"].append(rule_entry)
+            domain_entries.extend([address.strip() for address in addresses])
+        elif pattern == 'domain':
+            domain_entries.extend([address.strip() for address in addresses])
+        else:
+            rule_entry = {pattern: [address.strip() for address in addresses]}
+            result_rules["rules"].append(rule_entry)
+    # 删除 'domain_entries' 中的重复值
+    domain_entries = list(set(domain_entries))
+    if domain_entries:
+        result_rules["rules"].insert(0, {'domain': domain_entries})
 
     # 使用 output_directory 拼接完整路径
     file_name = os.path.join(output_directory, f"{os.path.basename(link).split('.')[0]}.json")
     with open(file_name, 'w', encoding='utf-8') as output_file:
-        json.dump(result_rules, output_file, ensure_ascii=False, indent=2)
+        json.dump(sort_dict(result_rules), output_file, ensure_ascii=False, indent=2)
 
+    srs_path = file_name.replace(".json", ".srs")
+    os.system(f"sing-box rule-set compile --output {srs_path} {file_name}")
     return file_name
 
 # 读取 links.txt 中的每个链接并生成对应的 JSON 文件
 with open("../links.txt", 'r') as links_file:
     links = links_file.read().splitlines()
+
+links = [l for l in links if l.strip() and not l.strip().startswith("#")]
 
 output_dir = "./"
 result_file_names = []
